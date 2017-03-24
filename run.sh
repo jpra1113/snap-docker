e7aceb4437c6da4735a49e820c9be22c2cf17f56
Timothy Chen <tnachen@gmail.com>
2017-03-24 22:46:38 +0000

    Use snap auto load plugin and task method instead

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 /usr/local/bin/init_snap
 
+python snap_init.py --config $1
+
 # Start Snap daemon
 snapteld --plugin-load-timeout 15 -t ${SNAP_TRUST_LEVEL} -l ${SNAP_LOG_LEVEL} -o '' &
 SNAP_PID=$(pidof snapteld)
-
-python snap_init.py --config $1
 
 wait $SNAP_PID
