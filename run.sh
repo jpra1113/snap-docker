f34439033c4fb929c25200376aedd5eb8a9c8c4e
Che-Yuan Liang <cheyuanl@andrew.cmu.edu>
2017-07-14 02:49:01 +0000

    Support retry on loading plugin

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-
 /usr/local/bin/init_snap
 
-python snap_init.py --config $1
+echo "args: $@"
 
-# Start Snap daemon
-snapteld --plugin-load-timeout 15 -t ${SNAP_TRUST_LEVEL} -l ${SNAP_LOG_LEVEL} -o '' &
-SNAP_PID=$(pidof snapteld)
+# Start Snapteld in background job
+echo "Starting snaptel deamon in background"
+snapteld -t ${SNAP_TRUST_LEVEL} -l ${SNAP_LOG_LEVEL} -o '' &
 
-wait $SNAP_PID
+# Start Snap_init.py in foreground job
+echo "Starting python in foreground"
+python snap_init.py --config $1 
+exit_status=$?
+echo "exit status: $exit_status"
+
+# Check exit status
+if [ $exit_status -ne "0" ]
+	then
+	exit 1 # Unable to load plugin OR create tasks after trials. Let k8s to restart this pod
+fi
+echo "Plugins are sucessfully loaded"
+echo "Tasks are sucessfully created"
+
+ps -l
+echo $(pidof snapteld)
+wait $(pidof snapteld)
